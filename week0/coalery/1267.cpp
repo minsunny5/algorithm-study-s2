adf54741f91ece1d15d80ecb13c0eb89dab5e723
러리 <doralife12@naver.com>
2021-05-07 00:25:08 +0900

    coalery/1267.cpp

---
+++ week0/coalery/1267.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+
+#pragma warning(disable: 4996)
+
+int main(void) {
+	int N;
+	scanf("%d", &N);
+
+	int Y = 0;
+	int M = 0;
+	for (int i = 0; i < N; i++) {
+		int tmp;
+		scanf("%d", &tmp);
+
+		Y += (tmp / 30) * 10 + 10;
+		M += (tmp / 60) * 15 + 15;
+	}
+
+	if (Y < M) printf("Y %d\n", Y);
+	else if (Y > M) printf("M %d\n", M);
+	else printf("Y M %d\n", Y);
+	return 0;
+}
