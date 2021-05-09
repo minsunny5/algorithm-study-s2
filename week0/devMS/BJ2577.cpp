4e35cea6de8834c970ec97456e3a7eef18e1d501
minsunny5 <minsunny5@gmail.com>
2021-05-10 01:51:35 +0900

    DevMS:Solve ABC Problem

--- week0/devMS/BJ2577.cpp
+++ week0/devMS/BJ2577.cpp
@@ -54,3 +54,27 @@ void count(int* arr)
     }
 }
 
+//러리님의 개쩌는 재귀 풀이
+/*
+#include <cstdio>
+
+#pragma warning(disable: 4996)
+
+int cnt[10];
+
+void solve(int current) {
+    if (current == 0) return;
+    cnt[current % 10]++;
+    solve(current / 10);
+}
+int main(void) {
+    int A, B, C;
+    scanf("%d %d %d", &A, &B, &C);
+    solve(A * B * C);
+
+    for (int i = 0; i < 10; i++)
+        printf("%d\n", cnt[i]);
+
+    return 0;
+}
+*/
