2376081d3993150d47a9992f0f777f28baba685f
nibble2 <nibblekk@gmail.com>
2021-05-10 21:20:08 +0900

    Fix - typo

--- week0/nibble/1267.js
+++ week0/nibble/1267.js
@@ -7,15 +7,9 @@ function s(input) {
     let ycall = 0;
     let mcall = 0;
     for (let i = 0; i < cnt; i++) {
-        console.log(times[i]);
         ycall += Math.ceil(Number(times[i]) / 30) * 10;
         mcall += Math.ceil(Number(times[i]) / 60) * 15;
     }
-
-    let fuck = 'Y M 90';
-    let fuckyou = "Y M " + String(mcall);
-
-    console.log(fuck === fuckyou);
 
     if (ycall > mcall) {
         console.log("M " + String(mcall))
