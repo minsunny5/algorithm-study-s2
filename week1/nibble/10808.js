2fb7d442641482a9851a8345dd908821bf4b8d25
nibble2 <nibblekk@gmail.com>
2021-05-16 19:20:26 +0900

    nibble : 10808.js 두번째 방법

--- week1/nibble/10808.js
+++ week1/nibble/10808.js
@@ -3,10 +3,17 @@ let input = fs.readFileSync('/dev/stdin').toString().split('');
 
 //O(N)
 let answer = new Array(26).fill(0);
+
+//첫번째 방법
 let map = input.map((char) => char.charCodeAt(0) - 97);
 
 for (let i = 0; i < map.length; i++) {
     answer[map[i]]++
 }
 
-console.log(answer.join(' '));
+//두번째 방법
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+input.forEach(i => answer[alphabet.indexOf(i)]++);
+
+
+console.log(answer.join(" "));
