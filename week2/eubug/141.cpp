32753ddff1e094a910da9ca8594a84362e115037
ipflegen <ipflegen@pm.me>
2021-05-18 20:01:55 +0900

    eubug: leetcode 141.cpp solved

--- week2/eubug/141.cpp
+++ week2/eubug/141.cpp
@@ -1,8 +1,7 @@
 /**
  * Runtime: 12 ms, faster than 64.66% of C++
- * Memory Usage: 8.1 MB, less than 13.75% of C++
+ * Memory Usage: 7.8 MB, less than 94.51% of C++
  */
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -19,17 +18,15 @@ class Solution {
       ListNode *turtle = head;
       ListNode *hare = head->next;
 
-      for(int i=0; i<10000; ++i) {
-        if(turtle == hare) return true;
+      while(turtle != hare) {
+        if(turtle == nullptr || hare == nullptr) return false;
 
         if(turtle) turtle = turtle->next;
-        else break;
 
         if(hare) hare = hare->next;
         if(hare) hare = hare->next;
-        else break;
       } 
 
-      return false;
+      return true;
     }
 };
