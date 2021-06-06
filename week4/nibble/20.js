a2de27224ec31f56cfe2cb9226f4df5db7de3419
nibble2 <vanillaleedana@gmail.com>
2021-06-06 16:03:10 +0900

    nibble: 20.js

---
+++ week4/nibble/20.js
@@ -0,0 +1,26 @@
+function solution(s) {
+    const stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        let c = s.charAt(i);
+        switch (c) {
+            case '(':
+                stack.push(')');
+                break;
+            case '{':
+                stack.push('}');
+                break;
+            case '[':
+                stack.push(']');
+                break;
+            default:
+                if (c !== stack.pop()) {
+                    return false;
+                }
+        }
+    }
+
+    return stack.length === 0;
+}
+
+console.log(solution("[{))"));
