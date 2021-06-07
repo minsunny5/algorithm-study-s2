44846f8efdd222c54df9e52f56834148bc713182
Ipflegen <ipflegen@pm.me>
2021-06-07 19:39:19 +0900

    eubug: 155 min stack O(1)

--- week4/eubug/155.cpp
+++ week4/eubug/155.cpp
@@ -1,4 +1,48 @@
 /*
+ * Runtime: 20 ms, faster than 81.89% of C++ online submissions for Min Stack.
+ * Memory Usage: 17.9 MB, less than 5.05% of C++ online submissions for Min Stack.
+ */
+
+class MinStack {
+  public:
+    int s1[30001];
+    int s2[30001];
+    int p1=0;
+    int p2=0;
+
+    MinStack() {}
+
+    void push(int val) {
+      s1[p1++]=val;
+      if(p2==0 || val <= s2[p2-1]) 
+        s2[p2++] = val;
+    }
+
+    void pop() {
+      if(s1[p1-1] == s2[p2-1]) --p2;
+      --p1;
+    }
+
+    int top() {
+      return s1[p1-1];
+    }
+
+    int getMin() {
+      return s2[p2-1];
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+
+#if 0
+/*
  * Runtime: 40 ms, faster than 13.32% of C++ online submissions 
  * Memory Usage: 17.3 MB, less than 5.79% of C++ online submissions 
  * */
@@ -43,3 +87,4 @@ class MinStack {
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
  */
+#endif
